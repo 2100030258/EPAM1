1f79705ef8a6c2b51085622a7a45e77593411f4b
2100030258 <110010631+2100030258@users.noreply.github.com>
2024-03-13 08:06:02 +0530

    Add files via upload

--- open_the_url.java
+++ open_the_url.java
@@ -2,12 +2,12 @@ package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class open_the_url {
     public static void main(String[] args) {
         WebDriver driver;
-        WebDriverManager.edgedriver().setup();
-        driver = new EdgeDriver();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         driver.get("https://lms.kluniversity.in/login/index.php");
         driver.findElement(By.id("username")).sendKeys("2100030258");
         driver.findElement(By.id("password")).sendKeys("///////");
